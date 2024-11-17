38e8fc71fcb6a3f74852ce07ae77e7496e065f59
akin <akn_kya@hotmail.com>
2024-11-18 01:59:08 +0300

    firsts

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Angular build dosyasını servise aç
-app.use(express.static(path.join(__dirname, 'dist/angular-tour-of-heroes')));
-
-// Tüm istekleri Angular uygulamasına yönlendir
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/angular-tour-of-heroes/index.html'));
-});
-
-// Heroku'nun sağladığı portu dinle
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use(express.static(__dirname + '/dist/angular-tour-of-heroes'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/angular-tour-of-heroes/index.html'));});
+app.listen(process.env.PORT || 8080);
