6254d625149a608fb8d5ff488703ea001d6885f4
akin <akn_kya@hotmail.com>
2024-11-18 01:35:26 +0300

    firsts

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const path = require('path');
+
+// Express uygulaması oluştur
 const app = express();
 
-// Angular dosyalarını serve et
-app.use(express.static(__dirname + '/dist/angular-tour-of-heroes'));
+// Angular statik dosyaları (SSR olmayan kısım için)
+app.use(express.static(path.join(__dirname, 'dist/angular-tour-of-heroes/browser')));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angular-tour-of-heroes/index.html'));
+// SSR için server tarafındaki render motorunu dahil et
+const ssrApp = require(path.join(__dirname, 'dist/angular-tour-of-heroes/server/main'));
+
+// Angular SSR middleware'i
+app.get('*', (req, res) => {
+  ssrApp.handle(req, res);
 });
 
-// Dinleme
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Sunucuyu başlat
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Angular app is running on port ${PORT}`);
 });
